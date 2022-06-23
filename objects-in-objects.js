a39e05424beeedaa28ef2e232d3f53ba0d97aa8d
Andrew <ayjtw7@gmail.com>
2022-06-23 18:45:49 -0400

    adding validators to calc

--- objects-in-objects.js
+++ objects-in-objects.js
@@ -21,3 +21,67 @@ let athlete = {
 }
 
 console.log(athlete.offStats.shootingPer);
+
+
+// -------------------------------------------
+
+
+let bikes = [{
+    brand: 'Golden Cycles',
+    year: 2022,
+    isFixedGear: true,
+    specs: {
+        wheelColor: 'Red',
+        frame: 54,
+        assembledOnDelivery: false,
+    }
+}]
+
+console.log(bikes[0].specs)
+
+const sportsTeams = [
+    ['Eagles', 'Sixers', 'Flyers', 'Phillies'],
+    ['Nets', 'Knicks', 'Yankees', 'Mets', 'Rangers', 'Islanders'],
+    ['Celtics', 'Patriots', 'Red Sox']
+];
+
+console.log(sportsTeams[0].push('Villanova'))
+console.log(sportsTeams[0])
+console.log(sportsTeams)
+
+
+// ------------------------------------------------------------------
+
+let products = [{
+        name: 'iPhone',
+        price: 799.99
+    },
+    {
+        name: 'Samsung Galaxy S10',
+        price: 900.0
+    }
+]
+
+products.push({
+    name: 'Motorola Razr',
+    price: 250.00
+})
+products.unshift({
+    name: 'test',
+    price: 'test'
+})
+console.log(products[0])
+
+// -------------------------------------------
+
+let course = {
+    name: 'Web Development',
+    type: ['full-time', 'part-time'],
+    topics: ['HTML/CSS & Responsive Design', 'JavaScript', 'MongoDB', 'Node', 'Express', 'React']
+
+}
+
+console.log(`This August I will be attending IronHack's ${course.type[0]} 
+${course.name} course, learning topics I am unfamiliar with, such as ${course.topics[4]}. `)
+
+console.log(course.topics.values()) 
--- switch-calculator.js
+++ switch-calculator.js
@@ -1,27 +1,49 @@
-// -------------------------------------------
+function calc(num1, num2, op) {
 
-function calculator(num1, num2, operator) {
+    /* Validators and conditions: 
+        makes sure user always passes three args */
+    console.log('Please provide both numbers and operator!');
+    if (!num1 || !num2 || !op) {
+        return;;
+    }
+
+    // makes sure user passes numeric values
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        console.log('Please add a numeric value only!')
+        return;
+    }
+
+    // make sure the user passes one of the valid operators (+, -, *, /)
+    if (op !== '+' && op !== '-' && op !== '*' && op !== '/') {
+        console.log('Please provide a valid operator.')
+        return;
+    }
+
+    /* initialize a variable that will hold the result */
     let result;
-    switch (operator) {
-        case '+':
-            result = num1 + num2;
-            break;
 
-        case '-':
-            result = num1 - num2;
-            break;
-
-        case '*':
-            result = num1 * num2;
-            break;
-
-        case '/':
+    switch (op) {
+        case "+":
             result = num1 + num2;
             break
-    }
-    // response = `${num1} ${operator} ${num2} = ${result}`;
-    return result
 
+        case "-":
+            result = num1 - num2;
+            break
+
+        case "/":
+            result = num1 / num2;
+            break
+
+        case "*":
+            result = num1 * num2;
+            break
+
+            // default:
+            //     return "Please select two numbers and an operator."
+
+    }
+    return result
 }
 
-console.log(calculator(7, 4, '*'))
+console.log(calc(5, 6, '*'))
